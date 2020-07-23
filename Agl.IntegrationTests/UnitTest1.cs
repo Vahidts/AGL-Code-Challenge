0af9e682618e2e2fb3ff5aecbd13c0f0e9e2f484
Vahid <vahidts@gmail.com>
2020-07-23 23:33:13 +1000

    initial commit. solution architecture

---
+++ Agl.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agl.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
